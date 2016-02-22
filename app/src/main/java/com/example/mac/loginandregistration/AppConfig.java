5fa350ff074482aafd6530f58c4383005e2d26af
aarongsy <gushengyang@sina.cn>
2016-02-22 10:18:52 +0000

    first change

---
+++ app/src/main/java/com/example/mac/loginandregistration/AppConfig.java
@@ -0,0 +1,13 @@
+package com.example.mac.loginandregistration;
+
+/**
+ * Created by mac on 22/02/2016.
+ */
+public class AppConfig {
+    // Server user login url
+        //public static String URL_LOGIN = "http://192.168.0.102:8888/android_login_api/login.php";
+    public static String URL_LOGIN = "https://www.nappyzap.com/androidInterface/checkLogin.php";
+
+    // Server user register url
+    public static String URL_REGISTER = "http://192.168.0.102:8888/android_login_api/register.php";
+}
